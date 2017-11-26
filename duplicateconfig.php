e6b2f10b47fa09c9ef8be7d6d034017d44ad4fdc
Nurul Azwani Binti Marwani <n.azwani1205@gmail.com>
2017-11-26 19:15:08 +0800

    Update duplicateconfig.php

--- duplicateconfig.php
+++ duplicateconfig.php
@@ -1,4 +1,19 @@
-<?php    
+<?php
+
+switch ($hostname) {
+  case 'domain.dev':
+    define('WP_ENV', 'development');
+    break;
+    
+   case 'staging.domain.com':
+    define('WP_ENV', 'development');
+    break;
+    
+   case 'www.domain.com':
+    default: ('WP_ENV', 'production');
+    break;
+}
+
   $HOST = 'ec2-54-221-207-143.compute-1.amazonaws.com';
   $PORT = '5432';
   $DATABASE_NAME = 'd1583ojbjir9jc';
